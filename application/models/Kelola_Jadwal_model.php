675b99c5faa202a62c6d666240b10fd1ed2a330d
4kari <mypasshidden@gmail.com>
2021-11-20 22:20:17 +0700

    update get jadwal

--- application/models/Kelola_Jadwal_model.php
+++ application/models/Kelola_Jadwal_model.php
@@ -33,16 +33,19 @@ class Kelola_Jadwal_model extends CI_Model{
         $hasil=[];
         foreach($data as $d){
             $id=$d['id_skripsi'];
-            // $skripsi = json_decode($this->curl->simple_get('http://10.5.12.21/skripsi/api/Skripsi/',array("id" => $id),array(CURLOPT_BUFFERSIZE => 10)),true);
-            $skripsi = json_decode($this->curl->simple_get('http://localhost/microservice/skripsi/api/Skripsi/',array("id" => $id),array(CURLOPT_BUFFERSIZE => 10)),true);
-            $ruangan = $this->db->get_where('Ruangan', ['id' => $d['ruangan']])->row_array();
-            $periode = $this->db->get_where('Periode', ['id' => $d['periode']])->row_array();
-            $waktu =  $this->db->get_where('Waktu', ['id' => $d['waktu']])->row_array();
-            $d['data_skripsi']=$skripsi['data'][0];            
-            if($ruangan){$d['kruangan']=$ruangan['ruangan'];}else{$d['kruangan']=$ruangan;}
-            if($periode){$d['kperiode']=$periode['periode'];}else{$d['kperiode']=$periode;}
-            if($waktu){$d['kwaktu']=$waktu['waktu'];}else{$d['kwaktu']=$waktu;}
-            array_push($hasil,$d);
+            $validasi = $this->db->get_where('validasi',['id_skripsi'=>$id]);
+            if(!$validasi){
+                // $skripsi = json_decode($this->curl->simple_get('http://10.5.12.21/skripsi/api/Skripsi/',array("id" => $id),array(CURLOPT_BUFFERSIZE => 10)),true);
+                $skripsi = json_decode($this->curl->simple_get('http://localhost/microservice/skripsi/api/Skripsi/',array("id" => $id),array(CURLOPT_BUFFERSIZE => 10)),true);
+                $ruangan = $this->db->get_where('Ruangan', ['id' => $d['ruangan']])->row_array();
+                $periode = $this->db->get_where('Periode', ['id' => $d['periode']])->row_array();
+                $waktu =  $this->db->get_where('Waktu', ['id' => $d['waktu']])->row_array();
+                $d['data_skripsi']=$skripsi['data'][0];            
+                if($ruangan){$d['kruangan']=$ruangan['ruangan'];}else{$d['kruangan']=$ruangan;}
+                if($periode){$d['kperiode']=$periode['periode'];}else{$d['kperiode']=$periode;}
+                if($waktu){$d['kwaktu']=$waktu['waktu'];}else{$d['kwaktu']=$waktu;}
+                array_push($hasil,$d);
+            }
         }
         return $hasil;
     }
