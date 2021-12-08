36678e62d8c6ffd3459e65fc11ff762bd49fccd8
4kari <mypasshidden@gmail.com>
2021-12-08 21:38:45 +0700

    perbaikan typo validasi

--- application/models/Validasi_model.php
+++ application/models/Validasi_model.php
@@ -19,10 +19,10 @@ class Validasi_model extends CI_Model{
     }
     public function updateValidasi($data,$id){
         $this->db->update('Validasi', $data, ['id' => $id]);
-        
         $val=$this->db->get_where('Validasi', ['id' => $id])->row_array();
+
         if($val['pembimbing_1'] && $val['pembimbing_2'] && $val['penguji_1'] && $val['penguji_2'] && $val['penguji_3']){
-            $jadwal = $this->db->getwhere('jadwal',['id'=>$val['id_jadwal']])->row_array();
+            $jadwal = $this->db->get_where('jadwal',['id'=>$val['id_jadwal']])->row_array();
             $skripsi = json_decode($this->curl->simple_get('http://localhost/microservice/skripsi/api/Skripsi/',array("id" => $jadwal['id_skripsi']),array(CURLOPT_BUFFERSIZE => 10)),true)['data'][0];
             if($skripsi['status']==2 || $skripsi['status']==5){$skripsi['status']=$skripsi['status']+1;}
             json_decode($this->curl->simple_put('http://localhost/microservice/skripsi/api/Skripsi/',$skripsi, array(CURLOPT_BUFFERSIZE => 10)),true);
